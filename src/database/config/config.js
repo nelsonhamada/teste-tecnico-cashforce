3f4a47092d08236e23eef50a4cd2d5aadfd18126
Nelson Hamada <fumiyuki.hamada@gmail.com>
2025-05-19 21:21:45 -0300

    [Chore]: update database name to 'cashforce_v3' in environment files and configuration

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DATABASE || 'cashforcedb',
+  database: process.env.MYSQL_DATABASE || 'cashforce_v3',
   host: process.env.MYSQL_HOST || 'database',
-  dialect: 'mysql',
+  dialect: 'mysql', 
 };
 
 module.exports = {
