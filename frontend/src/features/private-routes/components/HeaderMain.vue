38325ce0a4c8d340ac4260d8a307d1cdf4fd28f4
Nelson Hamada <fumiyuki.hamada@gmail.com>
2025-05-21 21:43:37 -0300

    [Feat]: add HeaderMain component and PrivateIndex view for private routes

---
+++ frontend/src/features/private-routes/components/HeaderMain.vue
@@ -0,0 +1,11 @@
+
+<template> 
+<v-app>
+
+    <v-app-bar>
+        <v-toolbar-title>My App</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn @click="() => console.log('foi')">Logout</v-btn>
+    </v-app-bar>
+</v-app>
+</template>
---
+++ frontend/src/features/private-routes/main/view/PrivateIndex.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from "@/features/private-routes/components/HeaderMain.vue";
+ 
+</script>
+
+<template>
+  <Header />
+    <v-main>
+        <router-view v-slot="{ Component }">
+          <Transition name="fade" mode="out-in">
+            <component :is="Component" />
+          </Transition>
+        </router-view>
+    </v-main>
+</template>
