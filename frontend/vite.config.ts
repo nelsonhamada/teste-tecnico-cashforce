20f19d9417eec6ac811a9134da99fd0f341587e6
Nelson Hamada <fumiyuki.hamada@gmail.com>
2025-05-28 21:32:54 -0300

    refactor: update README to clarify project stacks and setup instructions; adjust Vite config to comment out unused plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,19 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import eslint from "vite-plugin-eslint";
+// import eslint from "vite-plugin-eslint";
 import vuetify from "vite-plugin-vuetify";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     vuetify({
         autoImport: true,
       }),
-    eslint(),
+    // eslint(), usado apenas no desenvolvimento
   ],
   resolve: {
     alias: {
